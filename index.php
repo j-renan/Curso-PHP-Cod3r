8be36614bcd89823b68577b38572696bfb6f6569
Renan <desen.renan@gmail.com>
2021-02-15 08:44:06 -0300

    continuando repeticoes

--- index.php
+++ index.php
@@ -23,6 +23,21 @@
                         </a>
                     </li>
                     <li>
+                        <a href="exercicio.php?dir=repeticoes&file=desafio_for">
+                            Desafio For
+                        </a>
+                    </li>
+                    <li>
+                        <a href="exercicio.php?dir=repeticoes&file=foreach">
+                            Foreach
+                        </a>
+                    </li>
+                    <li>
+                        <a href="exercicio.php?dir=repeticoes&file=break_continue">
+                            Break/Continue
+                        </a>
+                    </li>
+                    
                 </ul>
             </div>
             <div class="modulo laranja">
